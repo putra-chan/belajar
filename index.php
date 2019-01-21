024c3ab17580a96ab3fdb06dc502590bd2d54f06
Putra <marshaa7x@gmail.com>
2019-01-21 23:43:14 +0700

    belajar

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="asset/css/normalize/normalize.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-top">
+          <h1><a href="#">Ahmad Syahputra</a></h1>
+        </div>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="layout/simple-layout.php">Project</a>
+            <a href="#">About Me</a>
+        </nav>
+      </div>
+    </header>
+
+    <div class="content">
+      <div class="wrapper-content">
+
+      </div>
+    </div>
+    <div class="footer" id="aboutme">
+
+    </div>
+  </body>
+</html>
