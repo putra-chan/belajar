18b0038cd8fb29437e7fbf6752a7dcbb64591348
Putra <marshaa7x@gmail.com>
2019-02-22 00:01:27 +0700

    created alat kopi layout and css

--- index.php
+++ index.php
@@ -48,6 +48,7 @@
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Alat Kopi<span class="caret"></span></a>
                   <ul class="dropdown-menu">
+                    <li><a href="layout/user/alat_kopi/alat_kopi.php">Semua Alat Kopi</a></li>
                     <li><a href="#">Dripper</a></li>
                     <li><a href="#">Espresso Maker</a></li>
                     <li><a href="#">French Press</a></li>
---
+++ layout/user/alat_kopi/alat_kopi.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jual Kopi | Jaya Kopi</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
+    <link rel="stylesheet" href="../../../asset/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../asset/css/normalize/normalize.css">
+    <link rel="stylesheet" href="../../../asset/css/css-layout/alat_kopi/alat_kopi.css">
+  </head>
+  <body>
+    <div class="wrapper-background-image">
+      <nav class="navbar navbar-default ">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-kopi" aria-expanded="false"></button>
+            <span class="sr-only"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <a href="../../../index.php" class="navbar-brand">Kopi Jaya</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse float-right" id="navbar-collapse-alat-kopi">
+            <ul class="nav navbar-nav list">
+              <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Biji Kopi<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="../biji-kopi/kopi.php">Semua Kopi</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Alat Kopi<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="alat_kopi.php">Semua Alat Kopi</a></li>
+                  <li><a href="#">Dripper</a></li>
+                  <li><a href="#">Espresso Maker</a></li>
+                  <li><a href="#">French Press</a></li>
+                  <li><a href="#">Grinder</a></li>
+                  <li><a href="#">Kettle</a></li>
+                  <li><a href="#">Paper Filter</a></li>
+                  <li><a href="#">Scale</a></li>
+                  <li><a href="#">Server</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Tentang Kami</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <main class="content">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-2">
+            <h3><b>Cari Produk</b></h3>
+            <div class="row">
+              <div class="col-xs-12">
+                <form class="" action="#!" method="post">
+                  <div class="form-pencarian">
+                    <input type="text" placeholder="Search" name="search" class="input">
+                  </div>
+                </form>
+              </div>
+              <div class="col-xs-12">
+                <h4><b>Filter Produk</b></h4>
+              </div>
+              <div class="col-xs-12">
+                <h4><b>Jenis Alat</b></h4>
+                <div class="row checkbox_position">
+                  <input type="checkbox" aria-label="...">
+                  <span>Dripper</span>
+                </div>
+              </div>
+              <div class="col-xs-12">
+                <h4><b>Brand</b></h4>
+                <div class="row checkbox_position">
+                  <input type="checkbox" aria-label="...">
+                  <span>Bonavita</span>
+                </div>
+              </div>
+          </div>
+
+        </div>
+        <div class="col-xs-12 col-md-9 col-md-offset-1">
+            <div class="row">
+              <div class="col-xs-12 col-md-4">
+                <div class="row">
+                  <div class="img-wrapper card">
+                    <a href="#">
+                      <img src="../../../asset/img/kopi.jpg" alt="kopi">
+                    </a>
+                  </div>
+                  <div class="price_and_button">
+                    <h3><b>Nama Kopinya</b></h3>
+                    <span class="price"><b>Rp. 7000</b></span>
+                    <a href="#" class="price_linkto_cart">
+                      <div class="links">
+                        Beli
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-md-4">
+
+              </div>
+              <div class="col-xs-12 col-md-4">
+
+              </div>
+            </div>
+        </div>
+      </div>
+    </main>
+
+    <script type="text/javascript" src="../../../asset/js/jquery.min.js"></script>
+    <script type="text/javascript" src="../../../asset/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
